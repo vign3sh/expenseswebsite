cbae4283b6e85f53330e8d47adfbf6ca52c9b797
Vignesh Singh <38295185+vign3sh@users.noreply.github.com>
2022-12-02 13:17:03 -0500

    Legend issues

--- expenseswebsite/static/js/expStats.js
+++ expenseswebsite/static/js/expStats.js
@@ -6,7 +6,6 @@ const renderChart = (data, labels) => {
       labels: labels,
       datasets: [
         {
-          label: "Last 6 months expenses",
           data: data,
           backgroundColor: [
             "rgba(255, 99, 132, 0.2)",
